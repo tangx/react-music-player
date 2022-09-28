c2cd1a877f9edb839e5b85847ca88f6d82a27b37
Frank (Xin) Tang <f.tang@aligntech.com>
2022-09-29 00:18:22 +0800

    展示列表

--- src/components/CenterCon/SongWrapper.tsx
+++ src/components/CenterCon/SongWrapper.tsx
@@ -6,23 +6,23 @@ interface SongWrapperProps {
   songs: Song[]
 }
 export default function SongWrapper(props: SongWrapperProps) {
+  const { songs } = props
   return (
     <div className='song_wrapper'>
       <ul className='song_list'>
-        <li>
-          <a href="" ></a>
-          <b>五月天</b>
-          <span>
-            <i></i>
-          </span>
-        </li>
-        <li >
-          <a href="" ></a>
-          <b>六月天</b>
-          <span>
-            <i></i>
-          </span>
-        </li>
+        {
+          songs.map((song) => {
+            return (
+              <li key={song.id}>
+                <a href=""></a>
+                <b>{song.name}</b>
+                <span>
+                  <i></i>
+                </span>
+              </li>
+            )
+          })
+        }
       </ul >
       <img src={line} className="switch_btn" alt="" />
     </div >
