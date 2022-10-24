f48dc30ea232b58162ef8074be1858d8e4837478
Frank (Xin) Tang <f.tang@aligntech.com>
2022-10-24 16:51:16 +0800

    12. 用户登录-登录界面

--- src/views/login/Login.jsx
+++ src/views/login/Login.jsx
@@ -1,7 +1,65 @@
 import React from 'react'
+import { Form, Input, Button, Checkbox } from 'antd'
+import { LockOutlined, UserOutlined } from '@ant-design/icons';
+import './Login.css';
+
 
 export default function Login() {
+
+  const handleOnFinished = (values) => {
+    console.log(values);
+  }
+
   return (
-    <div>Login</div>
+    <div
+      style={{ backgroundColor: "rgb(35,39,65)", height: "100%" }}
+    >
+      <div className='loginContainer'>
+        <div className='loginTitle'>全球发布管理系统</div>
+        <Form
+          name="normal_login"
+          className="login-form"
+          initialValues={{
+            remember: true,
+          }}
+          onFinish={handleOnFinished}
+        >
+          <Form.Item
+            name="username"
+            rules={[
+              {
+                required: true,
+                message: 'Please input your Username!',
+              },
+            ]}
+          >
+            <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
+          </Form.Item>
+
+          <Form.Item
+            name="password"
+            rules={[
+              {
+                required: true,
+                message: 'Please input your Password!',
+              },
+            ]}
+          >
+            <Input
+              prefix={<LockOutlined className="site-form-item-icon" />}
+              type="password"
+              placeholder="Password"
+            />
+          </Form.Item>
+
+          <Form.Item>
+            <Button type="primary" htmlType="submit" className="login-form-button">
+              Log in
+            </Button>
+            Or <a href="">register now!</a>
+          </Form.Item>
+        </Form>
+      </div>
+    </div>
   )
 }
