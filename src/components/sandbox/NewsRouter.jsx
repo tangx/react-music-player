fc0481a64e382b2dffe8ab845a32c142656c9a90
tangx <uyinn@live.com>
2022-11-01 07:44:05 +0800

    13. 路由权限管理-预设路由映射

--- src/components/sandbox/NewsRouter.jsx
+++ src/components/sandbox/NewsRouter.jsx
@@ -8,13 +8,28 @@ import UserList from '../../views/sandbox/user-manage/UserList'
 import RightList from '../../views/sandbox/right-manage/RightList'
 import RoleList from '../../views/sandbox/right-manage/RoleList'
 import NoPermission from '../../views/sandbox/nopermission/NoPermission'
-
+import NewsAdd from '../../views/sandbox/news-manage/NewsAdd'
+import NewsDraft from '../../views/sandbox/news-manage/NewsDraft'
+import NewsCategory from '../../views/sandbox/news-manage/NewsCategory'
+import Audit from '../../views/sandbox/audit-manage/Audit'
+import AuditList from '../../views/sandbox/audit-manage/AuditList'
+import Published from '../../views/sandbox/publish-manage/Published'
+import Unpublished from '../../views/sandbox/publish-manage/Unpublished'
+import Sunset from '../../views/sandbox/publish-manage/Sunset'
 
 const LocalRouterMap = {
   "/home": Home,
   "/user-manage/list": UserList,
   "/right-manage/right/list": RightList,
   "/right-manage/role/list": RoleList,
+  "/news-manage/add": NewsAdd,
+  "/news-manage/draft": NewsDraft,
+  "/news-manage/category": NewsCategory,
+  "/audit-manage/audit": Audit,
+  "/audit-manage/list": AuditList,
+  "/publish-manage/puhlished": Published,
+  "/publish-manage/unpublished": Unpublished,
+  "/publish-manage/sunset": Sunset,
 }
 
 export default function NewsRouter() {
